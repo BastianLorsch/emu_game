30127671dc9dd6f0717a3085e2811efb3815ee08
Bastian Lorsch <bastian.lorsch@t-online.de>
2025-03-16 17:14:10 +0100

    version 03.16.25 (enemmy weapon not fixed yet)

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -6,9 +6,8 @@ const JUMP_VELOCITY = -400.0
 @onready var player = get_node("../../Player")
 @onready var weapon = $weapon
 @onready var healthbar = $Healthbar 
-var is_attacking = false
-var attack_finished = false
 var health = 150 : set = _set_health
+var do_movement = true
 
 func _ready() -> void:
 	healthbar.init_health(health)
@@ -25,25 +24,14 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	
-	var do_movement = true
 	if player.position.x +30 > position.x and position.x > player.position.x -30:
 		do_movement = false
+	else:
+		do_movement = true
 	
 	if do_movement:
 		position.x = move_toward(position.x, player.position.x, delta * SPEED)
-	else: is_attacking = true
 	move_and_slide()
-	
-	#attack handling
-	if is_attacking and weapon.rotation_degrees > -30 and !attack_finished:
-		weapon.rotation_degrees -= 300 * delta
-	elif is_attacking and !attack_finished:
-		attack_finished = true
-	elif attack_finished and weapon.rotation_degrees < 70:
-		weapon.rotation_degrees += 100 * delta
-	else: 
-		attack_finished = false
-		is_attacking = false
 	
 func _on_hurtbox_area_entered(area: Area2D) -> void:
 	_set_health(10)
--- scripts/weapon.gd
+++ scripts/weapon.gd
@@ -1,6 +1,32 @@
 extends Area2D
 
+@onready var player_heart = get_node("../../../Player/player_heart")
+@onready var enemy = $".."
+@onready var attack_cooldown = $Timer
+var attacking = false
+var attack_ready = true
+
+func _process(delta: float) -> void:
+	if !attacking:
+		look_at(player_heart.global_position)
+	
+	if !enemy.do_movement and attack_ready and !attacking:
+		attacking = true
+	
+	if attacking:
+		attack_ready = false
+		#var position_difference_x = player_heart.position.x - position.x
+		#var position_difference_y = player_heart.position.y - position.y
+		
+		attack_cooldown.start()
+		attacking = false
+		position -= Vector2(100, 100)
+
 func _on_body_entered(body: Node2D) -> void:
 	var damage = 10
 	SignalBus.player_damaged.emit(damage)
 	print(body)
+
+func _on_timer_timeout() -> void:
+	attack_ready = true
+	
